bb4cb6df9dd64eea7fdc62c683ab7fc6c52e2aaf
karinaroseb <91501487+karinaroseb@users.noreply.github.com>
2021-11-26 23:08:20 -0800

    attempt

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+var $form = $('form#test-form'),
+ url = 'https://script.google.com/macros/s/AKfycbyoajJEJODTb4jN-KcJAnp7j-w-Odbn6UCtqJGMDIggOpbVWNtpkfB3KretRwJ1QgrY/exec'
+
+$('#submit-form').on('click', function(e) {
+e.preventDefault();
+var jqxhr = $.ajax({
+  url: url,
+  method: "GET",
+  dataType: "json",
+  data: $form.serializeObject()
+}).success(
+  // do something
+);
+})
